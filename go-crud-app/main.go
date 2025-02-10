aa5ef389da5670e1f1d64df8b48106dcc974ac97
MBatuhanOzer <batuhanozer1042@gmail.com>
2025-02-10 16:59:56 +0300

    initializing and adding dependencies

---
+++ go-crud-app/main.go
@@ -0,0 +1,18 @@
+package main 
+
+import (
+	"fmt"
+	"encoding/json"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/products", ProductsHandler)
+	r.HandleFunc("/articles", ArticlesHandler)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
